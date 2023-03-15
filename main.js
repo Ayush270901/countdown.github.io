032cd8ab90f90eaaec3076ca36c7c88548589216
Ayush270901 <77377043+Ayush270901@users.noreply.github.com>
2023-03-15 18:30:51 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", () => {
+  // Unix timestamp (in seconds) to count down to
+  let now = new Date().getTime();
+  
+  var cmDeadline = new Date("2023-03-20").getTime() / 1000  - 19800;
+  
+  var iplDeadline = new Date("2023-03-27").getTime() / 1000 + 14390;
+  // Set up FlipDown
+  new FlipDown(cmDeadline, "flipdown")
+
+    // Start the countdown
+    .start()
+
+    // Do something when the countdown ends
+    .ifEnded(() => {
+      console.log("The countdown has ended!");
+    });
+
+  new FlipDown(iplDeadline, "flipdown2")
+
+    // Start the countdown
+    .start()
+
+    // Do something when the countdown ends
+    .ifEnded(() => {
+      console.log("The countdown has ended!");
+    });
+});
