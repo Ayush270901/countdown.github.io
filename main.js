890ad4a3762a471bbe453cf028e659175f14d0a8
Ayush270901 <77377043+Ayush270901@users.noreply.github.com>
2023-03-20 12:10:26 +0530

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
   // Unix timestamp (in seconds) to count down to
   let now = new Date().getTime();
   
-  var cmDeadline = new Date("2023-03-20").getTime() / 1000  - 19800;
+  var cmDeadline = new Date("2023-03-21").getTime() / 1000 - 47720;
   
   var iplDeadline = new Date("2023-03-27").getTime() / 1000 + 14390;
   // Set up FlipDown
